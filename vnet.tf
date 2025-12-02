a782a9be7604a77c045769750b13de9998ca5ecd
unknown <satyanarayanaangadala4@gmail.com>
2025-12-02 20:00:57 +0530

    tages

--- vnet.tf
+++ vnet.tf
@@ -3,6 +3,10 @@ name = "vnet-01"
 address_space=["10.1.0.0/16"]
 resource_group_name=azurerm_resource_group.rg.name
 location = "east-us"
+tags= {
+    name="satya-rg"
+ }
+ 
 }
 
 resource "azurem_subnet" "name" {
