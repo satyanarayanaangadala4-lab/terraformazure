0dabd333be4ae3dceb2f592e6ffb441d2e636b78
unknown <satyanarayanaangadala4@gmail.com>
2025-12-02 18:04:18 +0530

    subnet code

--- vnet.tf
+++ vnet.tf
@@ -4,3 +4,10 @@ address_space=["10.1.0.0/16"]
 resource_group_name=azurerm_resource_group.rg.name
 location = "east-us"
 }
+
+resource "azurem_subnet" "name" {
+name="subnet-01"
+address_prefixes=["10.1.1.0/24"]
+resource_group_name=  azurerm_resource_group.rg.name
+virtual_network_name=azurerm_virtual_network.name.name
+}
